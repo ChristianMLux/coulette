414e97b05fb82a5bedd2330d56eac5a77f73a536
ChristianMLux <christian.m.lux@gmail.com>
2021-07-14 18:54:53 +0200

    issues: toggle backgroundcolor #78

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-var welcome = "Welcome to coulette";
-console.log("hello ", welcome);
+let welcome = "Welcome to coulette";
+console.log(welcome);
+
+const changeColorBtn = document.querySelector("#changeColorBtn");
+
+if (changeColorBtn) {
+  changeColorBtn.addEventListener("click", function () {
+    const color = "orange";
+    const header = document.querySelector("header");
+    const currentBGColor = header.style.backgroundColor.toLowerCase();
+    if (currentBGColor !== "orange") {
+      header.style.backgroundColor = color;
+    } else {
+      header.style.backgroundColor = null;
+    }
+  });
+} else {
+  console.error("sorry, can't find the button, please check the name again");
+}
