0b38ec45d9a17a52097cbf126ced8a80ed936732
ChristianMLux <christian.m.lux@gmail.com>
2021-07-27 11:35:15 +0200

    feature: e2e test: save color

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -1,11 +1,26 @@
+// Welcome to Coulette
+//
+// this app can generate random colors and save them in a list
 describe("rnd color generator", () => {
-  it("should show initial generated color", () => {
+  beforeEach(() => {
     cy.visit("/");
+  });
+
+  it("should show initial generated color", () => {
     cy.get("[data-cy=colorCodeP").should("exist");
   });
+
   it("should generate new color", () => {
-    cy.visit("/");
     cy.get("[data-cy=generateColorBtn]").click();
-    cy.get("[data-cy=colorCodeP]").should("exist");
+    cy.get("[data-cy=colorCodeP]").contains("#").should("exist");
+  });
+
+  it("should generate a list item with the colorcode", () => {
+    cy.get("[data-cy=saveColorBtn]").click();
+
+    cy.get("[data-cy=colorList]").should("have.length", 1);
+    cy.get("[data-cy=colorLi]").contains("#").should("exist");
+    cy.get("[data-cy=deleteBtn").should("exist");
+    cy.get("[data-cy=saveColorBtn]").should("be.disabled");
   });
 });
